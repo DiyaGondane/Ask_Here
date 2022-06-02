b7d7be7cd5ed5ca586013d3f772257f24609e2c2
Diya Omprakash Gondane <diyagondane2001@gmail.com>
2022-06-02 18:08:26 +0530

    category model

---
+++ lib/models/category.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Category {
+  final String topic;
+  final String tag;
+  final Color color;
+
+  const Category({
+    @required this.topic,
+    this.tag,
+    this.color = Colors.orange,
+
+  });
+}
